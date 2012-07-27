21e2a651efb47e1fdffb7aca48afdab28781d8bc
unknown <Mortaro@.(none)>
2012-07-27 19:40:51 -0300

    extra routes

--- lib/toy-locomotive/initializer.rb
+++ lib/toy-locomotive/initializer.rb
@@ -1,16 +1,19 @@
 module ToyLocomotive
   class Engine < Rails::Engine
-    initializer 'toy_locomotive.initialize', :after=> :disable_dependency_loading do |app|
+    initializer 'toy_locomotive.initialize', :after => :disable_dependency_loading do |app|
       Dir["#{Rails.root}/app/models/*.rb"].each do |file|
         require file
         AttributeObserver.new file.split('/').last.split('.').first.classify.constantize
       end
+      controllers = []
       Dir["#{Rails.root}/app/controllers/*.rb"].each do |file|
         require file
-        const = file.split('/').last.split('.').first.classify.constantize.append_filters!
+        controllers << file.split('/').last.split('.').first.classify.constantize
+        controllers.last.append_filters!
       end
       Rails.application.class.routes.draw do
         ToyLocomotive.routes.each {|route| match route[:path] => "#{route[:controller]}##{route[:action]}", as: route[:as], via: route[:method]}
+        controllers.each {|controller| controller._route.call if controller._route}
       end
     end
   end
---
+++ lib/toy-locomotive/router/block.rb
@@ -0,0 +1,11 @@
+module ToyLocomotive::Router::Block
+
+  mattr_accessor :_route
+
+  def route &block
+    @_route = &block
+  end
+
+end
+
+ActionController::Base.extend ToyLocomotive::Router::Block
